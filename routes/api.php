7bf83832edd03ccc8d8492405a5340de5a1c9fb6
Rahma <rahmaaliyyah29@gmail.com>
2026-01-21 11:31:11 +0700

    Add API routes for all endpoints

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TopUpController;
+use App\Http\Controllers\Api\PaymentController;
+use App\Http\Controllers\Api\TransferController;
+use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\Api\ProfileController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/topup', [TopUpController::class, 'topUp']);
+    Route::post('/pay', [PaymentController::class, 'pay']);
+    Route::post('/transfer', [TransferController::class, 'transfer']);
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    Route::put('/profile', [ProfileController::class, 'update']);
+});
