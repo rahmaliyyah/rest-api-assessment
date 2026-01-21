ea72983347a458417a6181fec18cf1ac9c5e9096
Rahma <rahmaaliyyah29@gmail.com>
2026-01-21 11:08:09 +0700

    Add TopUpController for top up balance endpoint

---
+++ app/Http/Controllers/Api/TopUpController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\TopUp;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class TopUpController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function topUp(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'amount' => 'required|numeric|min:1',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => $validator->errors()->first()
+            ], 400);
+        }
+
+        $user = auth()->user();
+
+        try {
+            DB::beginTransaction();
+
+            $balanceBefore = $user->balance;
+            $balanceAfter = $balanceBefore + $request->amount;
+
+            $topUp = TopUp::create([
+                'user_id' => $user->user_id,
+                'amount_top_up' => $request->amount,
+                'balance_before' => $balanceBefore,
+                'balance_after' => $balanceAfter,
+            ]);
+
+            $user->update(['balance' => $balanceAfter]);
+
+            DB::commit();
+
+            return response()->json([
+                'status' => 'SUCCESS',
+                'result' => [
+                    'top_up_id' => $topUp->top_up_id,
+                    'amount_top_up' => (float) $topUp->amount_top_up,
+                    'balance_before' => (float) $topUp->balance_before,
+                    'balance_after' => (float) $topUp->balance_after,
+                    'created_date' => $topUp->created_date->format('Y-m-d H:i:s'),
+                ]
+            ], 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'message' => 'Top up failed'
+            ], 500);
+        }
+    }
+}
